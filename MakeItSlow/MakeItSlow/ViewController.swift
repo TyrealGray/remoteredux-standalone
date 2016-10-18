84f91b80e7c3425e04cb71d5a54ad8b4900b0c75
tyrealgray <tyrealgray@ymail.com>
2016-10-18 21:56:02 +0200

    add Xcode project
    Former-commit-id: 2218debed1986e0aaa74d2cd73c4ee3d985eddb4 [formerly c3f18427d51b69bed04ecfec7b5eb98d9798de1c]
    Former-commit-id: b6fd2aa9ab8ded1fd92e658f5f1eba30be9ed864

---
+++ MakeItSlow/MakeItSlow/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MakeItSlow
+//
+//  Created by Tyreal Gray on 10/18/16.
+//  Copyright © 2016 Tyreal Gray. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    var wk:WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
