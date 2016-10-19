007c0d224983aaa74c752abecd51da2b61f04b64
Tyreal Gray <tyreal.gray@kartenmacherei.de>
2016-10-19 10:10:32 +0200

    change Xcode code, try to set js hook

--- MakeItSlow/MakeItSlow/ViewController.swift
+++ MakeItSlow/MakeItSlow/ViewController.swift
@@ -2,16 +2,19 @@
 //  ViewController.swift
 //  MakeItSlow
 //
-//  Created by Tyreal Gray on 10/18/16.
+//  Created by Tyreal Gray on 10/19/16.
 //  Copyright © 2016 Tyreal Gray. All rights reserved.
 //
 
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate,WKUIDelegate {
     
-    var wk:WKWebView!
+    
+    
+    var wk: WKWebView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,14 +26,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
+    private typealias wkNavigationDelegate = ViewController
+    
+    
+    override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
         self.wk = WKWebView(frame: self.view.frame)
-        self.wk.load(NSURLRequest(url: NSURL(string: "http://www.baidu.com/")! as URL) as URLRequest)
+        self.wk.loadRequest(NSURLRequest(URL: NSURL(string: "https://tyrealgray.github.io/MakeItSlow/")!))
         self.view.addSubview(self.wk)
+        
+        self.wk.navigationDelegate = self
+        self.wk.UIDelegate = self
+        
     }
 
-
 }
+
+private typealias wkNavigationDelegate = ViewController
+extension wkNavigationDelegate {
+    
+    func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
+        NSLog(error.debugDescription)
+    }
+    func webView(webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: NSError) {
+        NSLog(error.debugDescription)
+    }
+}
+
+private typealias wkUIDelegate = ViewController
+extension wkUIDelegate {
+    
+    func webView(webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: () -> Void) {
+        let ac = UIAlertController(title: webView.title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        ac.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: { (aa) -> Void in
+            completionHandler()
+        }))
+        self.presentViewController(ac, animated: true, completion: nil)
+    }
+}
+
 
