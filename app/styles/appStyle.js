d9aaa807a1d2cfab7f191586ab23631113a0fbf8
tyrealgray <tyrealgray@ymail.com>
2016-12-04 14:12:54 +0100

    build
    Former-commit-id: 34179683e6320469ae63bbf1b1162aedf7e4179c [formerly f3a7b887c14e58695aa8ad979f45eb9954c200c4]
    Former-commit-id: 93cb53d3ca862036eb8ac5dead64feae2a36e699

---
+++ app/styles/appStyle.js
@@ -0,0 +1,10 @@
+import {
+	StyleSheet
+} from 'react-native';
+
+import base from './base';
+import layout from './layout';
+
+const styles = StyleSheet.create(Object.assign({},base,layout));
+
+export default styles;
