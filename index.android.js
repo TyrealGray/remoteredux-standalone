7bc8731c03fa88a5584d32850ea5553a60502e5a
tyrealgray <tyrealgray@ymail.com>
2017-01-12 23:10:15 +0100

    combine to one code

--- index.android.js
+++ index.android.js
@@ -6,48 +6,21 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	Platform
 } from 'react-native';
 
-export default class MakeItSlow extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+import App from './app/entry';
+
+AppRegistry.registerComponent('MakeItSlow', () => App);
+
+if (Platform.OS == 'web') {
+	var app = document.createElement('div');
+	document.body.appendChild(app);
+	AppRegistry.runApplication('App', {
+		rootTag: app
+	})
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('MakeItSlow', () => MakeItSlow);
