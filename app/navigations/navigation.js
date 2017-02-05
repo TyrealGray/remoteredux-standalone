641e690c1fbe6e1f5c5ed0b9fa9c4445db68b003
tyrealgray <tyrealgray@ymail.com>
2017-02-05 23:29:26 +0100

    implement navigator
    Former-commit-id: f092b6b50d472d7fbaaefb9a55ae918d47e4676f [formerly ba38373e185811f3a0e6aea7797f6f8da29ee669]
    Former-commit-id: f25e92c71fca81e1236b2ba35c8187e418acc458

--- app/navigations/navigation.js
+++ app/navigations/navigation.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import { Text, Navigator } from 'react-native';
 
+import Guide from '../screens/guide';
+
 export default class Navigation extends Component {
 
   constructor(props){
     super(props);
   }
 
+  renderScene(route, nav){
+
+    const page = <Guide/>;
+
+    switch(route.page){
+      case 'home':
+      return page;
+      default:
+      break;
+    }
+  }
+
   render() {
     return (
       <Navigator
-        initialRoute={{ title: 'Awesome Scene', index: 0 }}
-        renderScene={(route, navigator) =>
-          <Text>Hello {route.title}!</Text>
+        initialRoute={{ page: 'home'}}
+        renderScene={
+          (route, nav) => this.renderScene(route, nav)
         }
         style={{padding: 100}}
       />
--- app/screens/guide.js
+++ app/screens/guide.js
@@ -1,3 +1,15 @@
 /**
  * TODO: implement this screen to guide user setup his deadline
  */
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Guide extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <Text>Guide page</Text>;
+    }
+}
