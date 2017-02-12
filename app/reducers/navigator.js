4fd1848105140054a49de788d589a6e6c98d8ba7
tyrealgray <tyrealgray@ymail.com>
2017-02-12 17:04:50 +0100

    add redux
    Former-commit-id: 97681d226988400812f7fefb7a38b518a4974ff4 [formerly 19c02f0f07e587feec995c3fa789d82317a2cdfd]
    Former-commit-id: 2875d88dc697c72571744bba034887f49bf43b6c

--- app/reducers/navigator.js
+++ app/reducers/navigator.js
@@ -1,14 +1,32 @@
 import * as types from '../actions/actionTypes';
 
+import Guide from '../screens/guide';
+
+function ggetPage(name){
+    let page = null;
+    
+    switch(name){
+        case 'home':
+        page = Guide;
+        default:
+        break;
+    }
+
+    return page;
+}
+
 const initialState = {
-    page:'home'
+    name: 'home',
+    page: Guide
 };
 
 export default function navigator(state = initialState, actions = {}){
     switch (action.type){
         case types.NAV_PUSH:
          return{
-             ...state
+             ...state,
+             name: action.page,
+             page: getPage(actions.page)
          }
         default:
         return state;
