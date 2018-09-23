4e6d2dc1d24966002032b62f4752b8493a51ea7f
TyrealGray <tyrealgray@ymail.com>
2018-09-23 13:17:34 +0200

    disable warning

--- bin/remoteredux.js
+++ bin/remoteredux.js
@@ -4,7 +4,7 @@ var spawn = require('cross-spawn');
 var argv = process.argv.slice(2);
 var path = require('path');
 
-
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true;
 var result = spawn.sync(
 	electron,
 	[path.join( __dirname, "remotedev" )].concat(argv),
